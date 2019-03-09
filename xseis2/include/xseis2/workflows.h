2e9c924a6883f5b91f92d28074340af3da48b3a2
Philippe Dales <pdales@gmail.com>
2019-03-09 16:03:13 +0100

    moved some hardcoded settings out of interloc and changed call signature

--- xseis2/include/xseis2/workflows.h
+++ xseis2/include/xseis2/workflows.h
@@ -1,10 +1,5 @@
 #pragma once
 
-// #include <iostream>
-// #include <vector>
-// #include <iterator>
-// #include <numeric>
-// #include <string>
 #include "xseis2/core.h"
 #include "xseis2/logger.h"
 #include "xseis2/hdf5.h"
@@ -19,112 +14,141 @@
 
 namespace xs {
 
-void WFSearchOnePhase(Array2D<float>& rdat, float sr, Array2D<float>& stalocs, Vector<uint16_t>& chanmap, VecOfSpans<uint16_t> ttable, int64_t* outbuf, int debug, std::string& npzfile) 
-{	
+// Pre-process, cross-correlate and beamform raw_data using provided traveltime table.
+// The provided traveltimes determine which phase (e.g P,S) is searched for
+void WFSearchOnePhase(
+	Array2D<float>& raw_data, // 2d array of seismic data (nchan x nsamples)
+	float samplerate,
+	Array2D<float>& station_locations, // each row is station location xyz
+	Vector<uint16_t>& channel_map, // maps station locations to raw data channels
+	VecOfSpans<uint16_t> ttable, // each row contains traveltime to all grid points for one station
+	int64_t* outbuf, // output buffer that results are written to be ready in python
+	std::vector<float>& whiten_corner_freqs, // 4 corner frequencies to whiten between
+	float pair_dist_min, // uses only correlation pairs between dist_min and dist_max
+	float pair_dist_max,
+	float cc_smooth_length_sec, // length to absolute value smooth ccfs before beamforming
+	int debug_lvl, // debug_lvl level | 0: disabled, 1: logging 2: logging + data dump to debug_file
+	std::string& debug_file //
+	)
+{
 
-	std::vector<float> cfreqs {40, 50, 350, 360};
-	float dist_min = 0;
-	float dist_max = 2000;
-	uint32_t const smooth_cc_wlen = 0.005 * sr;
+	uint32_t const smooth_cc_wlen = cc_smooth_length_sec * samplerate;
 
 	auto logger = xs::Logger();
-	if (debug > 0) logger.log("Start");
+	if (debug_lvl > 0) logger.log("Start");
 
 	// PREPROC //////////////////////////////////////////////////////////////
-	auto dat = xs::ZeroPad(rdat, rdat.ncol() * 2);
-	if (debug > 0) logger.log("create padded");
+	auto zpad_data = xs::ZeroPad(raw_data, raw_data.ncol() * 2);
+	if (debug_lvl > 0) logger.log("create padded");
 
-	auto fdat = xs::FFTAndWhiten(dat, sr, cfreqs);
-	if (debug > 0) logger.log("fft");
-	if (debug == 2) xs::NpzSave(npzfile, "dat_preproc", dat.rows(), "w");
-	if (debug == 2) logger.log("save dat");
+	auto freq_data = xs::FFTAndWhiten(zpad_data, samplerate, whiten_corner_freqs);
+	if (debug_lvl > 0) logger.log("fft");
+	if (debug_lvl == 2) xs::NpzSave(debug_file, "dat_preproc", zpad_data.rows(), "w");
+	if (debug_lvl == 2) logger.log("save dat");
 
 	// KEYGEN //////////////////////////////////////////////////////////////
-	auto groups = xs::GroupChannels(chanmap.span());
-	auto keys = xs::Arange<uint16_t>(0, stalocs.nrow());
+	auto groups = xs::GroupChannels(channel_map.span());
+	auto keys = xs::Arange<uint16_t>(0, station_locations.nrow());
 	auto allpairs = xs::UniquePairs(keys);
-	// auto pairs = allpairs.rows();
-	auto pairs = xs::DistFiltPairs(allpairs.rows(), stalocs.rows(), dist_min, dist_max);
-	if (debug > 0) printf("using %lu pairs of %lu total\n", pairs.size(), allpairs.nrow());
-	if (debug > 0) logger.log("build sta pairs");
-	if (debug == 2) xs::NpzSave(npzfile, "sta_pairs", pairs, "a");
-	if (debug == 2) logger.log("save sta pairs");
+	auto pairs = xs::DistFiltPairs(allpairs.rows(), station_locations.rows(), pair_dist_min, pair_dist_max);
+	if (debug_lvl > 0) printf("using %lu pairs of %lu total\n", pairs.size(), allpairs.nrow());
+	if (debug_lvl > 0) logger.log("build sta pairs");
+	if (debug_lvl == 2) xs::NpzSave(debug_file, "sta_pairs", pairs, "a");
+	if (debug_lvl == 2) logger.log("save sta pairs");
 
 
 	// XCORRS //////////////////////////////////////////////////////////////
-	auto ccdat = xs::Array2D<float>(pairs.size(), dat.ncol());	
-	xs::XCorrCombineChans(fdat, groups, pairs, ccdat, smooth_cc_wlen);
-	if (debug > 0) logger.log("xcorr");
-	if (debug == 2) xs::NpzSave(npzfile, "dat_cc", ccdat.rows(), "a");
-	if (debug == 2) logger.log("save ccfs");	
+	auto cc_data = xs::Array2D<float>(pairs.size(), zpad_data.ncol());
+	xs::XCorrCombineChans(freq_data, groups, pairs, cc_data, smooth_cc_wlen);
+	if (debug_lvl > 0) logger.log("xcorr");
+	if (debug_lvl == 2) xs::NpzSave(debug_file, "dat_cc", cc_data.rows(), "a");
+	if (debug_lvl == 2) logger.log("save ccfs");
 
-	// xs::IsValidTTable(pairs, ttable, ccdat.ncol());
-	// auto dd = xs::DistDiffPairs(pairs, stalocs.rows());
+	// xs::IsValidTTable(pairs, ttable, cczpad_data.ncol());
+	// auto dd = xs::DistDiffPairs(pairs, station_locations.rows());
 	// std::cout << "max dist_diff: " << xs::Max(gsl::make_span(dd)) << "\n";
 
 	// SEARCH //////////////////////////////////////////////////////////////
 	auto power = xs::Vector<float>(ttable[0].size());
-	xs::InterLocBlocks(ccdat.rows(), pairs, ttable, power.span());
-	
-	// auto power = xs::InterLoc(ccdat.rows(), pairs, ttable);
-	// auto power = xs::InterLocBad(ccdat.rows(), pairs, ttable);
-	size_t imax = xs::ArgMax(power.span());
-	float vmax = xs::Max(power.span());
-	if (debug > 0) logger.log("interloc");
-	if (debug == 2) xs::NpzSave(npzfile, "grid_power", power.span(), "a");
-	if (debug == 2) logger.log("save grid");
+	xs::InterLocBlocks(cc_data.rows(), pairs, ttable, power.span());
 
-	auto rmaxes = xs::RowMaxes(ccdat.rows());
+	// auto power = xs::InterLoc(cczpad_data.rows(), pairs, ttable);
+	// auto power = xs::InterLocBad(cczpad_data.rows(), pairs, ttable);
+	size_t argmax = xs::ArgMax(power.span());
+	float valmax = xs::Max(power.span());
+	if (debug_lvl > 0) logger.log("interloc");
+	if (debug_lvl == 2) xs::NpzSave(debug_file, "grid_power", power.span(), "a");
+	if (debug_lvl == 2) logger.log("save grid");
+
+	auto rmaxes = xs::RowMaxes(cc_data.rows());
 	float max_theor = xs::Mean(gsl::make_span(rmaxes));
-	// if (debug > 0) logger.log("cc theor max");	
-	float peak_align = vmax / max_theor * 100.0f;
-	if (debug > 0) logger.log("cc maxes");
-	if (debug > 0) printf("(max_grid / max_theor)= %f / %f = %f%%\n", vmax, max_theor, peak_align);
+	// if (debug_lvl > 0) logger.log("cc theor max");
+	float peak_align = valmax / max_theor * 100.0f;
+	if (debug_lvl > 0) logger.log("cc maxes");
+	if (debug_lvl > 0) printf("(max_grid / max_theor)= %f / %f = %f%%\n", valmax, max_theor, peak_align);
 
 
 	// ROLL FOR ORIGIN_TIME ////////////////////////////////////////////////////
 	std::vector<uint16_t> wtt;
-	for(auto&& row : ttable) wtt.push_back(row[imax]);		
-	if (debug > 0) logger.log("tts to source");
-	if (debug==2) xs::NpzSave(npzfile, "tts_src", gsl::make_span(wtt), "a");
-	if (debug==2) logger.log("save wtt");
+	for(auto&& row : ttable) wtt.push_back(row[argmax]);
+	if (debug_lvl > 0) logger.log("tts to source");
+	if (debug_lvl==2) xs::NpzSave(debug_file, "tts_src", gsl::make_span(wtt), "a");
+	if (debug_lvl==2) logger.log("save wtt");
 
 	// xs::RollAndStack(dat, groups, wtt.span());
-	xs::Envelope(dat.rows());
-	if (debug > 0) logger.log("envelope");
+	xs::Envelope(zpad_data.rows());
+	if (debug_lvl > 0) logger.log("envelope");
 
 	// get origin time by shifting and stacking, len(stack) = 2 * len(sig)
-	auto stack = xs::RollAndStack(dat.rows(), groups, gsl::make_span(wtt));	
-	int64_t otime = xs::ArgMax(stack.span()) - static_cast<int64_t>(stack.size() / 2);	
-	if (debug > 0) logger.log("stack");
-	if (debug==2) xs::NpzSave(npzfile, "dat_stack", stack.span(), "a");
+	auto stack = xs::RollAndStack(zpad_data.rows(), groups, gsl::make_span(wtt));
+	int64_t otime = xs::ArgMax(stack.span()) - static_cast<int64_t>(stack.size() / 2);
+	if (debug_lvl > 0) logger.log("stack");
+	if (debug_lvl==2) xs::NpzSave(debug_file, "dat_stack", stack.span(), "a");
 
-	if (debug==2) {
-		xs::RollSigs(dat.rows(), groups, gsl::make_span(wtt));
+	if (debug_lvl==2) {
+		xs::RollSigs(zpad_data.rows(), groups, gsl::make_span(wtt));
 		logger.log("roll");
-		xs::NpzSave(npzfile, "dat_rolled", dat.rows(), "a");
+		xs::NpzSave(debug_file, "dat_rolled", zpad_data.rows(), "a");
 	}
 
-	// xs::RollSigs(dat.rows(), groups, gsl::make_span(wtt));
-	// if (debug > 0) logger.log("roll");
-	// auto stack = xs::StackSigs(dat.rows());
-	// if (debug > 0) logger.log("stack");	
-	// if (debug==2) xs::NpzSave(npzfile, "dat_rolled", dat.rows(), "a");
-	// if (debug==2) xs::NpzSave(npzfile, "dat_stack", stack.span(), "a");
+	// xs::RollSigs(zpad_data.rows(), groups, gsl::make_span(wtt));
+	// if (debug_lvl > 0) logger.log("roll");
+	// auto stack = xs::StackSigs(zpad_data.rows());
+	// if (debug_lvl > 0) logger.log("stack");
+	// if (debug_lvl==2) xs::NpzSave(debug_file, "dat_rolled", zpad_data.rows(), "a");
+	// if (debug_lvl==2) xs::NpzSave(debug_file, "dat_stack", stack.span(), "a");
 
-	if (debug > 0) printf("[gmax] %f [imax] %lu [ot_imax] %ld\n", vmax, imax, otime);
+	if (debug_lvl > 0) printf("[gmax] %f [argmax] %lu [ot_argmax] %ld\n", valmax, argmax, otime);
 
-	outbuf[0] = static_cast<int64_t>(vmax * 10000); // max power scaled
-	outbuf[1] = imax; // tt grid argmax
+	outbuf[0] = static_cast<int64_t>(valmax * 10000); // max power scaled
+	outbuf[1] = argmax; // tt grid argmax
 	outbuf[2] = otime; // origin time argmax
 
-	if (debug > 0) logger.summary();
+	if (debug_lvl > 0) logger.summary();
 
 }
 
-void SearchOnePhase(float* rawdat_p, uint32_t nchan, uint32_t npts, float sr, float* stalocs_p, uint32_t nsta, uint16_t* chanmap_p, std::vector<uint16_t*>& tt_ptrs, uint32_t ngrid, int64_t* outbuf, uint32_t nthreads, int debug, std::string& npzfile) 
+// Called directly by cython, takes pointers and builds appropriate data structures
+void SearchOnePhase(
+	float* raw_data_ptr, uint32_t nchan, uint32_t nsample, // ptr to raw data (size = nchan x nsamp)
+	float samplerate,
+	float* station_locations_ptr, uint32_t nsta, // ptr to station locs (size = nsta x 3)
+	uint16_t* channel_map_ptr, // ptr to channel map (size = nchan)
+	std::vector<uint16_t*>& ttable_row_ptrs, uint32_t ngrid,
+	int64_t* outbuf,
+	std::vector<float>& whiten_corner_freqs, // 4 corner frequencies to whiten between
+	float pair_dist_min, // uses only correlation pairs between dist_min and dist_max
+	float pair_dist_max,
+	float cc_smooth_length_sec, // length to absolute value smooth ccfs before beamforming
+	uint32_t nthreads,
+	int debug_lvl,
+	std::string& debug_file
+	)
 {
 
+	// std::vector<float> whiten_corner_freqs {40, 50, 350, 360};
+	// float pair_dist_min = 0;
+	// float pair_dist_max = 2000;
 	omp_set_num_threads(nthreads);
 	std::string const HOME = std::getenv("SPP_COMMON");
 	std::string file_wisdom = HOME + "fftw3_wisdom.txt";
@@ -132,16 +156,23 @@ void SearchOnePhase(float* rawdat_p, uint32_t nchan, uint32_t npts, float sr, fl
 	// std::cout << "file_wisdom: " << file_wisdom << "\n";
 
 	xs::VecOfSpans<uint16_t> ttable;
-	for(auto&& ptr : tt_ptrs) ttable.push_back(gsl::make_span(ptr, ngrid));
-	
-	auto dat = xs::Array2D<float>(rawdat_p, nchan, npts, true); // copies data
-	auto stalocs = xs::Array2D<float>(stalocs_p, nsta, 3, false);
-	auto chanmap = xs::Vector<uint16_t>(chanmap_p, nchan);
+	for(auto&& ptr : ttable_row_ptrs) ttable.push_back(gsl::make_span(ptr, ngrid));
 
-	xs::WFSearchOnePhase(dat, sr, stalocs, chanmap, ttable, outbuf, debug, npzfile);
-	
+	auto dat = xs::Array2D<float>(raw_data_ptr, nchan, nsample, true); // copies data
+	auto station_locations = xs::Array2D<float>(station_locations_ptr, nsta, 3, false);
+	auto channel_map = xs::Vector<uint16_t>(channel_map_ptr, nchan);
+
+	xs::WFSearchOnePhase(dat, samplerate,
+		station_locations, channel_map,
+		ttable,
+		outbuf,
+		whiten_corner_freqs,
+		pair_dist_min, pair_dist_max,
+		cc_smooth_length_sec,
+		debug_lvl, debug_file);
+
 	fftwf_export_wisdom_to_filename(&file_wisdom[0]);
-	
+
 }
 
 }
--- xseis2/xspy.pyx
+++ xseis2/xspy.pyx
@@ -13,18 +13,36 @@ ctypedef uint16_t* uint16_ptr # pointer workaround
 
 cdef extern from "xseis2/workflows.h" namespace "xs":
 
-	void SearchOnePhase(float* rawdat_p, uint32_t nchan, uint32_t npts, float sr, float* stalocs_p, uint32_t nsta, uint16_t* chanmap_p, vector[uint16_ptr]& tt_ptrs_vec, uint32_t ngrid, int64_t* outbuf, uint32_t nthreads, int debug, string& file_out)
+	void SearchOnePhase(
+		float* raw_data_ptr, uint32_t nchan, uint32_t nsample, # ptr to raw data (size = nchan x nsamp)
+		float samplerate,
+		float* station_locations_ptr, uint32_t nsta, # ptr to station locs (size = nsta x 3)
+		uint16_t* channel_map_ptr, # ptr to channel map (size = nchan)
+		vector[uint16_ptr]& ttable_row_ptrs, uint32_t ngrid,
+		int64_t* outbuf,
+		vector[float]& whiten_corner_freqs, # 4 corner frequencies to whiten between
+		float pair_dist_min, # uses only correlation pairs between dist_min and dist_max
+		float pair_dist_max,
+		float cc_smooth_length_sec, # length to absolute value smooth ccfs before beamforming
+		uint32_t nthreads,
+		int debug_lvl,
+		string& debug_file
+		)
 
 
 def pySearchOnePhase(np.ndarray[np.float32_t, ndim=2] data,
-					sr,
+					samplerate,
 					np.ndarray[np.uint16_t, ndim=1] chanmap,
-					np.ndarray[np.float32_t, ndim=2] stalocs,					
+					np.ndarray[np.float32_t, ndim=2] stalocs,
 					np.ndarray[np.int64_t, ndim=1] tt_ptrs,
 					ngrid,
+					np.ndarray[np.float32_t, ndim=1] whiten_corner_freqs,
+					pair_dist_min,
+					pair_dist_max,
+					cc_smooth_length_sec,
 					nthreads,
-					debug,
-					outfile
+					debug_lvl,
+					debug_file
 			   ):
 
 	stalocs = np.ascontiguousarray(stalocs)
@@ -33,24 +51,33 @@ def pySearchOnePhase(np.ndarray[np.float32_t, ndim=2] data,
 	assert(data.shape[0] == chanmap.shape[0])
 	assert(tt_ptrs.shape[0] == stalocs.shape[0])
 
+	# convert np array to std::vector
+	cdef vector[float] corner_freqs_vec
+	for i in range(whiten_corner_freqs.shape[0]):
+		corner_freqs_vec.push_back(<float>whiten_corner_freqs[i])
+
 	# buil vec of c-pointers from casted python ptrs
 	cdef vector[uint16_ptr] tt_ptrs_vec
 	for i in range(tt_ptrs.shape[0]):
 		tt_ptrs_vec.push_back(<uint16_t*>tt_ptrs[i])
 
-	cdef string outfile_str = outfile.encode('UTF-8')
+	cdef string debug_file_str = debug_file.encode('UTF-8')
 	cdef np.ndarray[np.int64_t, ndim=1] out = np.empty(3, dtype=np.int64)
 	# print("{0:x}".format(<unsigned long>&stalocs[0, 0]))
 
 	SearchOnePhase(&data[0, 0], data.shape[0], data.shape[1],
-					sr,
+					samplerate,
 					&stalocs[0, 0], stalocs.shape[0],
-					&chanmap[0],					
+					&chanmap[0],
 					tt_ptrs_vec, ngrid,
 					&out[0],
+					corner_freqs_vec,
+					pair_dist_min,
+					pair_dist_max,
+					cc_smooth_length_sec,
 					nthreads,
-					debug,
-					outfile_str
+					debug_lvl,
+					debug_file_str
 					)
 
 	out_list = [float(out[0]) / 10000., out[1], out[2]]
